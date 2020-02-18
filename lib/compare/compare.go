36c4e174e97556ca048b74c23452f8df18db7736
dmitri <deemok@gmail.com>
2020-02-18 18:33:48 +0100

    Format non-printable characters (newline, tab) to make diffs more visible

--- lib/compare/compare.go
+++ lib/compare/compare.go
@@ -17,9 +17,12 @@ limitations under the License.
 package compare
 
 import (
+	"bytes"
 	"reflect"
 	"runtime/debug"
 	"sort"
+	"strings"
+	"unicode"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/kylelemons/godebug/diff"
@@ -45,38 +48,15 @@ func (checker *deepEqualsChecker) Check(params []interface{}, names []string) (r
 	if len(params) != 2 {
 		panic("at least two params are required")
 	}
-
 	if isString(params[0]) && isString(params[1]) {
 		info := *checker.CheckerInfo
 		return (&stringEqualsChecker{
 			CheckerInfo: &info,
 		}).Check(params, names)
 	}
-
 	result = reflect.DeepEqual(params[0], params[1])
 	if !result {
 		error = Diff(params[0], params[1])
-	}
-	return result, error
-}
-
-// StringEquals is a gocheck checker that compares two strings
-var StringEquals check.Checker = &stringEqualsChecker{
-	&check.CheckerInfo{Name: "StringEquals", Params: []string{"obtained", "expected"}},
-}
-
-// Check expects two slices in params (obtained and expected).
-// The slices are sorted before comparison, hence they are expected to implement sort.Interface.
-// If comparison fails, it returns a readable diff in error.
-// Implements gocheck checker interface
-func (checker *stringEqualsChecker) Check(params []interface{}, names []string) (result bool, error string) {
-	obtained := params[0].(string)
-	expected := params[1].(string)
-	result = reflect.DeepEqual(obtained, expected)
-	if !result {
-		dmp := diffmatchpatch.New()
-		diffs := dmp.DiffMain(obtained, expected, false)
-		error = dmp.DiffPrettyText(diffs)
 	}
 	return result, error
 }
@@ -104,6 +84,69 @@ func (checker *sliceEqualsChecker) Check(params []interface{}, names []string) (
 	return result, error
 }
 
+// StringEquals is a gocheck checker that compares two strings
+var StringEquals check.Checker = &stringEqualsChecker{
+	&check.CheckerInfo{Name: "StringEquals", Params: []string{"obtained", "expected"}},
+}
+
+// Check expects two slices in params (obtained and expected).
+// The slices are sorted before comparison, hence they are expected to implement sort.Interface.
+// If comparison fails, it returns a readable diff in error.
+// Implements gocheck checker interface
+func (checker *stringEqualsChecker) Check(params []interface{}, names []string) (result bool, error string) {
+	obtained := params[0].(string)
+	expected := params[1].(string)
+	result = reflect.DeepEqual(obtained, expected)
+	if !result {
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(obtained, expected, false)
+		error = DiffPrettyText(*dmp, diffs)
+	}
+	return result, error
+}
+
+func DiffPrettyText(dmp diffmatchpatch.DiffMatchPatch, diffs []diffmatchpatch.Diff) string {
+	var buf bytes.Buffer
+	for _, diff := range diffs {
+		text := diff.Text
+		switch diff.Type {
+		case diffmatchpatch.DiffInsert:
+			_, _ = buf.WriteString("\x1b[32m")
+			if nonPrintable(text) {
+				text = replaceNonPrintable(text)
+			}
+			_, _ = buf.WriteString(text)
+			_, _ = buf.WriteString("\x1b[0m")
+		case diffmatchpatch.DiffDelete:
+			_, _ = buf.WriteString("\x1b[31m")
+			if nonPrintable(text) {
+				text = replaceNonPrintable(text)
+			}
+			_, _ = buf.WriteString(text)
+			_, _ = buf.WriteString("\x1b[0m")
+		case diffmatchpatch.DiffEqual:
+			_, _ = buf.WriteString(text)
+		}
+	}
+
+	return buf.String()
+}
+
+func nonPrintable(s string) bool {
+	for _, c := range s {
+		if unicode.IsPrint(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func replaceNonPrintable(s string) string {
+	return replacer.Replace(s)
+}
+
+var replacer = strings.NewReplacer("\n", "⤶\n", "\r", "⤶\r", "\t", "⇥")
+
 // Diff returns user friendly difference between two objects
 func Diff(a, b interface{}) string {
 	d := &spew.ConfigState{Indent: " ", DisableMethods: true, DisablePointerMethods: true, DisablePointerAddresses: true}
@@ -120,11 +163,11 @@ type deepEqualsChecker struct {
 	*check.CheckerInfo
 }
 
-type stringEqualsChecker struct {
+type sliceEqualsChecker struct {
 	*check.CheckerInfo
 }
 
-type sliceEqualsChecker struct {
+type stringEqualsChecker struct {
 	*check.CheckerInfo
 }
 
